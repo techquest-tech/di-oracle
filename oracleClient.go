f6f108835ca3ef316ae063a2474326c846d0d80c
armenpn <armenpn@gmail.com>
2022-10-26 15:42:44 +0800

    set pool to default 30mins timeout.

--- oracleClient.go
+++ oracleClient.go
@@ -19,6 +19,8 @@ type OracleSetting struct {
 	Service       string
 	User          string
 	Password      string
+	MaxLifetime   time.Duration
+	MaxIdletime   time.Duration
 	Mustconnected bool // should test oracle connection when bootup
 }
 
@@ -26,9 +28,11 @@ func init() {
 	core.GetContainer().Provide(func(logger *zap.Logger) (*sql.DB, error) {
 		settings := viper.Sub("oracle")
 		oracleSetting := &OracleSetting{
-			Host:    "127.0.0.1",
-			Port:    1521,
-			Service: "xe",
+			Host:        "127.0.0.1",
+			Port:        1521,
+			Service:     "xe",
+			MaxIdletime: 5 * time.Minute,
+			MaxLifetime: 30 * time.Minute,
 		}
 		if settings != nil {
 			settings.Unmarshal(oracleSetting)
@@ -43,7 +47,10 @@ func init() {
 			logger.Error("connect to oracle failed.", zap.Error(err))
 			return nil, err
 		}
-		logger.Info("connect to oracle", zap.String("URI", fullurl))
+		db.SetConnMaxIdleTime(oracleSetting.MaxIdletime)
+		db.SetConnMaxLifetime(oracleSetting.MaxLifetime)
+
+		logger.Info("connect to oracle", zap.String("URI", fullurl), zap.Duration("maxLifetime", oracleSetting.MaxLifetime))
 
 		if oracleSetting.Mustconnected {
 			ctx, cancel := context.WithTimeout(context.TODO(), 2*time.Second)
@@ -63,9 +70,12 @@ func init() {
 	core.Container.Provide(ToGorm)
 }
 
-//make sql.DB to gorm.DB, but only support gorm.Raw, nothing else.
+// make sql.DB to gorm.DB, but only support gorm.Raw, nothing else.
 func ToGorm(db *sql.DB, logger *zap.Logger) (*gorm.DB, error) {
-	logger.Info("convert sql.DB to gorm.DB")
+	logger.Debug("convert sql.DB to gorm.DB")
+	// cfg := &gorm.Config{
+	// 	PrepareStmt: true,
+	// }
 	t, err := gorm.Open(mysql.New(mysql.Config{
 		SkipInitializeWithVersion: true,
 		ServerVersion:             "8.0",
@@ -74,5 +84,8 @@ func ToGorm(db *sql.DB, logger *zap.Logger) (*gorm.DB, error) {
 	if err != nil {
 		logger.Error("init gorm DB failed.", zap.Error(err))
 	}
+
+	logger.Info("connect to oracle done.")
+
 	return t, err
 }
